3348473d2070f08ed13318d1d69703a8efcef2bf
Sneha1622 <snehajaiswal@eng.rizvi.edu.in>
2024-12-07 21:20:38 +0530

    Add files via upload

---
+++ exp1task1.c
@@ -0,0 +1,16 @@
+/*
+Expt 1 - Task 1
+Module 1 : Fundamentals of C Programming
+Aim: WAP to print your name, age, class, division
+and UIN on the screen. Use escape sequences
+to properly format the output.
+*/
+#include<stdio.h>
+int main(){
+printf("Full Name\t: sneha Jaiswal\n");
+printf("Age\t\t: 18\n");
+printf("Class\t\t: FE\n");
+printf("Division\t: A\n");
+printf("UIN\t\t: 241A024\n");
+return 0;
+}
